38a77bab4d3f19e6b3efdd04dfc65102d0730afd
Manish-Sudhir <manishsudhir8@gmail.com>
2023-08-16 20:01:15 +0100

    Updated modellin.py to return best model metrics

--- modelling.py
+++ modelling.py
@@ -7,6 +7,9 @@ from tabular_data import load_airbnb
 import os
 import joblib
 import json
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+
 
 def preprocess_data(features, labels):
     # Preprocess your data here (imputation, scaling, etc.)
@@ -68,41 +71,119 @@ def save_model(model, hyperparameters, metrics, folder):
     with open(metrics_file, 'w') as f:
         json.dump(metrics, f, indent=4)
 
+def evaluate_all_models(features, labels):
+    # Preprocess data and create validation set
+    X_train, y_train, X_val, y_val, X_test, y_test = preprocess_data(features, labels)
+    
+    # Define hyperparameter grids for each model
+    decision_tree_param_grid = {
+        'max_depth': [None, 10, 20],
+        'min_samples_split': [2, 5, 10]
+    }
+    random_forest_param_grid = {
+        'n_estimators': [50, 100, 200],
+        'max_depth': [None, 10, 20],
+        'min_samples_split': [2, 5, 10]
+    }
+    gradient_boosting_param_grid = {
+        'n_estimators': [50, 100, 200],
+        'learning_rate': [0.01, 0.1, 0.2],
+        'max_depth': [3, 5, 10]
+    }
+    sgd_param_grid = {
+        'alpha': [0.0001, 0.001, 0.01],
+        'max_iter': [1000, 2000]
+    }
+    
+    # Models to evaluate
+    models = [
+        DecisionTreeRegressor(),
+        RandomForestRegressor(),
+        GradientBoostingRegressor(),
+        SGDRegressor()
+    ]
+    
+    for model in models:
+        if isinstance(model, DecisionTreeRegressor):
+            param_grid = decision_tree_param_grid
+            model_name = "decision_tree"
+        elif isinstance(model, RandomForestRegressor):
+            param_grid = random_forest_param_grid
+            model_name = "random_forest"
+        elif isinstance(model, GradientBoostingRegressor):
+            param_grid = gradient_boosting_param_grid
+            model_name = "gradient_boosting"
+        elif isinstance(model, SGDRegressor):
+            param_grid = sgd_param_grid
+            model_name = "stochhastic_gradient_descent"
+        else:
+            continue  # Skip unknown models
+        
+        # Tune and train the model using GridSearchCV
+        best_model, best_hyperparameters = tune_regression_model_hyperparameters(
+            model, param_grid, X_train, y_train, X_val, y_val
+        )
+        
+        # Train the best model using the full training data
+        final_model = train_model(best_model.__class__, best_hyperparameters, X_train, y_train)
+        
+        # Evaluate the final model on the test set
+        test_rmse, test_r2, mse = evaluate_model(final_model, X_test, y_test)
+        
+        # # Save the model, hyperparameters, and metrics
+        folder_path = f"models/regression/{model_name}"
+        save_model(final_model, best_hyperparameters, {"test_RMSE": test_rmse, "test_R2": test_r2, "MSE": mse}, folder_path)
+        
+        print(f"{model_name.capitalize()} - Best Hyperparameters:", best_hyperparameters)
+        print("Test RMSE:", test_rmse)
+        print("Test R2:", test_r2)
+        print("Mean Squared Error:", mse)
+
+def find_best_model(models_folder):
+    model_folders = [f for f in os.listdir(models_folder) if os.path.isdir(os.path.join(models_folder, f))]
+
+    best_model = None
+    best_metrics = None
+    best_hyperparameters = None
+    best_rmse = float('inf')
+
+    for model_name in model_folders:
+        model_folder = os.path.join(models_folder, model_name)
+        metrics_file = os.path.join(model_folder, 'metrics.json')
+
+        if os.path.exists(metrics_file):
+            with open(metrics_file, 'r') as f:
+                metrics = json.load(f)
+                rmse = metrics['test_RMSE']
+
+                if rmse < best_rmse:
+                    best_rmse = rmse
+                    best_metrics = metrics
+
+                    # Load hyperparameters
+                    hyperparameters_file = os.path.join(model_folder, 'hyperparameters.json')
+                    with open(hyperparameters_file, 'r') as hf:
+                        best_hyperparameters = json.load(hf)
+
+                    # Load model
+                    model_file = os.path.join(model_folder, 'model.joblib')
+                    best_model = joblib.load(model_file)
+
+    return best_model, best_hyperparameters, best_metrics
 
 def main():
     # Load the dataset with 'price_night' as the label
     features, labels = load_airbnb()
 
-    # Preprocess data and create validation set
-    X_train, y_train, X_val, y_val, X_test, y_test = preprocess_data(features, labels)
+    # Evaluate all models
+    evaluate_all_models(features, labels)
 
-    # Define hyperparameter grid for SGDRegressor
-    param_grid = {
-        'alpha': [0.0001, 0.001, 0.01],
-        'max_iter': [1000, 2000]
-        # Add more hyperparameters and values as needed
-    }
+    # Find the best model
+    best_model, best_hyperparameters, best_metrics = find_best_model("models/regression")
 
-    # Create and tune the model using GridSearchCV
-    sgd_model = SGDRegressor()
-    best_model, best_hyperparameters = tune_regression_model_hyperparameters(
-        sgd_model, param_grid, X_train, y_train, X_val, y_val
-    )
-    
-    # Train the best model using the full training data
-    final_model = train_model(best_model.__class__, best_hyperparameters, X_train, y_train)
-    
-    # Evaluate the final model on the test set
-    test_rmse, test_r2, mse = evaluate_model(final_model, X_test, y_test)
-    
-    # Save the model, hyperparameters, and metrics
-    folder_path = "models/regression/linear_regression"
-    save_model(final_model, best_hyperparameters, {"test_RMSE": test_rmse, "test_R2": test_r2, "MSE": mse}, folder_path)
-
+    print("Best Model:", best_model)
     print("Best Hyperparameters:", best_hyperparameters)
-    print("Test RMSE:", test_rmse)
-    print("Test R2:", test_r2)
-    print("Mean Squared Error:", mse)
+    print("Best Metrics:", best_metrics)
 
 if __name__ == "__main__":
     main()
